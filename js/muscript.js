96ba864c12f8ac2ee51e4cc9ffeff71c35acb5b5
Ali Yousefi <alifordevelpment@gmail.com>
2023-07-19 14:31:57 +0430

    finished comment section and start best price

--- js/muscript.js
+++ js/muscript.js
@@ -72,7 +72,45 @@ $(".slider2").slick({
     // instead of a settings object
   ],
 });
+
 $(".slider3").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  rtl: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+$(".slider2-comment").slick({
   dots: true,
   infinite: true,
   speed: 500,
